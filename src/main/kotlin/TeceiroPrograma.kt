8bbd4c038727722f151513c8e0ec0727d7542654
Roberto <rober.autom@gmail.com>
2022-09-11 06:16:59 -0300

    Initial commit

---
+++ src/main/kotlin/TeceiroPrograma.kt
@@ -0,0 +1,26 @@
+/**
+ * Este programa responderá a seguinte questão:
+ * Como Calcular quantos anos de vida a pessoa tem?
+ */
+
+fun main() {
+
+    //Utlizar CamelCase
+    //Dados de entrada
+    var anoDeNascimento = 1981
+    var anoAtual = 2022
+
+    //Processamento
+    var anosDeVida = anoAtual - anoDeNascimento
+
+    //Dados de Saída /Resultado
+
+    println("* Como Calcular quantos anos de vida a pessoa tem?")
+    println("- Ano de Nascimento: $anoDeNascimento")
+    println("- Ano Atual........: $anoAtual")
+    println("- Resultado........: $anosDeVida")
+    println("  Você tem $anosDeVida de vida.")
+
+
+
+}
